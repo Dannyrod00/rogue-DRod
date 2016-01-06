76659906d24daf7342fbc8e1d53daf02a89a57c6
danny <dannyrodriguez822@gmail.com>
2016-01-06 09:18:53 -0800

    vid 18

---
+++ Assets/scripts/BoardController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardController : MonoBehaviour {
+
+	public int collum;
+	public int rows;
+
+	public GameObject floor;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
