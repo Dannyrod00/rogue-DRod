c2015819df9cff398d533c82118da931b9228d86
danny <dannyrodriguez822@gmail.com>
2016-01-06 09:12:06 -0800

    vid 17

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController Instance;
+
+	void Awake () {
+		if(Instance != null && Instance != this){
+			Destroy(gameObject);
+			return;
+
+		}
+
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+
+	}
+	
+
+	void Update () {
+	
+	}
+}
