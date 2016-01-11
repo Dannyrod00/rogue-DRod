33175215d37742d79f699ab5c1d19180a97e49a2
danny <dannyrodriguez822@gmail.com>
2016-01-11 10:11:31 -0800

    vid 25

--- Assets/scripts/BoardController.cs
+++ Assets/scripts/BoardController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BoardController : MonoBehaviour {
 
@@ -7,15 +8,27 @@ public class BoardController : MonoBehaviour {
 	public int rows;
 
 	public GameObject[] floors;
+	public GameObject[] outerWalls;
 
 	private Transform gameBoard;
+	private List<Vector3> ObstacleGrid;
 
-	void Start () {
-	
+	void Awake () {
+		ObstaclesGrid = new List<Vector3>();
 	}
 	
 	void Update () {
 	
+	}
+
+	private void InitializeObstaclePositions(){
+		obstaclesGrid.Clear ();
+
+		for (int x = 2; x < columns -2; x++) {
+			for(int y = 2; y < rows -2; y++){
+				obstaclesGrid.Add(new Vector3(x, y, 0f));
+			}
+		}
 	}
 
 	private void setUpGameBoard(){
@@ -23,7 +36,12 @@ public class BoardController : MonoBehaviour {
 
 		for(int x = 0; x < columns; x++){
 			for(int y = 0; y < rows; y++){
-				GameObject selectedTile = floors[Random.Range(0, floors.Length)];
+				GameObject selectedTile;
+				if(x == 0 || y == 0 || x == columns - 1 || y == rows - 1){
+				selectedTile = outerWalls[Random.Range(0,outerWalls.Length)];
+				}else{
+				selectedTile = floors[Random.Range(0, floors.Length)];
+				}
 				GameObject floorTile = (GameObject)Instantiate(selectedTile, new Vector3(x, y, 0f), Quaternion.identity);
 				floorTile.transform.SetParent(gameBoard);
 			}
@@ -32,7 +50,23 @@ public class BoardController : MonoBehaviour {
 
 	}
 
+	public void SetRandomObstaclesOnGrid(GameObject[] obstaclesArray, int minimum, int maximum){
+		int obstacleCount = Random.Range(minimum, maximum +1);
+
+		if(obstacleCount > obstaclesGrid.Count){
+			obstacleCount = obstaclesGrid.Count;
+
+		}
+
+		for (int index = 0; index < obstacleCount; index++) {
+			
+		
+		}
+	 
+	}
+
 	public void SetupLevel(){
+		InitializeObstaclePositions ();
 		setUpGameBoard();
 
 	}
