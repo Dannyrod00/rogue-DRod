b31fb6794ba5c198e4ab2a674f16fc4a4d3f1268
danny <dannyrodriguez822@gmail.com>
2016-01-28 09:06:51 -0800

    vid32

---
+++ Assets/scripts/MovingObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour {
+
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rigidBody
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
