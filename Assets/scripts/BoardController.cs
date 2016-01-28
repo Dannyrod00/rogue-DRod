d51336b41a306384a1a6c010e6883f314ec9da5f
danny <dannyrodriguez822@gmail.com>
2016-01-28 08:57:42 -0800

    finished milestone 6

--- Assets/scripts/BoardController.cs
+++ Assets/scripts/BoardController.cs
@@ -9,12 +9,16 @@ public class BoardController : MonoBehaviour {
 
 	public GameObject[] floors;
 	public GameObject[] outerWalls;
+	public GameObject[] wallObstacles;
+	public GameObject[] foodItems;
+	public GameObject[] enemies;
+	public GameObject exit;
 
 	private Transform gameBoard;
-	private List<Vector3> ObstacleGrid;
+	private List<Vector3> obstacleGrid;
 
 	void Awake () {
-		ObstaclesGrid = new List<Vector3>();
+		obstacleGrid = new List<Vector3>();
 	}
 	
 	void Update () {
@@ -22,16 +26,16 @@ public class BoardController : MonoBehaviour {
 	}
 
 	private void InitializeObstaclePositions(){
-		obstaclesGrid.Clear ();
+		obstacleGrid.Clear ();
 
 		for (int x = 2; x < columns -2; x++) {
 			for(int y = 2; y < rows -2; y++){
-				obstaclesGrid.Add(new Vector3(x, y, 0f));
+				obstacleGrid.Add(new Vector3(x, y, 0f));
 			}
 		}
 	}
 
-	private void setUpGameBoard(){
+	private void SetUpGameBoard(){
 		gameBoard = new GameObject("Game Board").transform;
 
 		for(int x = 0; x < columns; x++){
@@ -53,21 +57,33 @@ public class BoardController : MonoBehaviour {
 	public void SetRandomObstaclesOnGrid(GameObject[] obstaclesArray, int minimum, int maximum){
 		int obstacleCount = Random.Range(minimum, maximum +1);
 
-		if(obstacleCount > obstaclesGrid.Count){
-			obstacleCount = obstaclesGrid.Count;
+		if(obstacleCount > obstacleGrid.Count){
+			obstacleCount = obstacleGrid.Count;
 
 		}
 
 		for (int index = 0; index < obstacleCount; index++) {
-			
+			GameObject selectedObstacle = obstaclesArray[Random.Range(0, obstaclesArray.Length)];
+			Instantiate(selectedObstacle, SelectGridPosition(), Quaternion.identity);
 		
 		}
-	 
+	}
+
+	private Vector3 SelectGridPosition(){
+		int randomIndex = Random.Range (0, obstacleGrid.Count);
+		Vector3 randomPosition = obstacleGrid[randomIndex];
+		obstacleGrid.RemoveAt (randomIndex);
+		return randomPosition;
+	
 	}
 
 	public void SetupLevel(){
 		InitializeObstaclePositions ();
-		setUpGameBoard();
+		SetUpGameBoard();
+		SetRandomObstaclesOnGrid (wallObstacles, 3, 9);
+		SetRandomObstaclesOnGrid (foodItems, 1, 5);
+		SetRandomObstaclesOnGrid (enemies, 2, 5);
+		Instatiate (exit, Vector3 (colums - 2, rows - 2, 0f),Quaternion.identity);
 
 	}
 
