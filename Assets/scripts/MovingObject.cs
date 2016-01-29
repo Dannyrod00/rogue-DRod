b47bee3bcf78db0b0f6fc4ba5b808cbc38af4ba7
danny <dannyrodriguez822@gmail.com>
2016-01-29 08:49:23 -0800

    vid 34

--- Assets/scripts/MovingObject.cs
+++ Assets/scripts/MovingObject.cs
@@ -8,9 +8,20 @@ public class MovingObject : MonoBehaviour {
 
 
 	void Start () {
+		boxCollider = GetComponent<BoxCollider2D> ();
+		rigidBody = GetComponent<Rigidbody2D>();
 	
 	}
-	
+
+	protected bool canObjectMove(int xDirection, int yDirection){
+		Vector2 startPosition = rigidBody.position;
+		Vector2 endPosition = startPosition + new Vector2 (int xDirection, int yDirection);
+
+		Rigidbody.MovePosition(endPosition);
+
+		return true;
+	}
+
 	void Update () {
 	
 	}
