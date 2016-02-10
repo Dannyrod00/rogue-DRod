0f6be7c1b659c5e9a771fdc7ffd8295f76014ce1
danny <dannyrodriguez822@gmail.com>
2016-02-09 16:32:02 -0800

    vid 47

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MovingObject {
+
+	private Transform player;
+
+	protected override void Start () {
+		GameController.Instance.AddEnemyToList (this);
+		player = GameObject.FindGameObjectsWithTag ("Player").transform;
+		base.Start ();
+	}
+
+	void Update () {
+	
+	}
+}
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,15 +2,31 @@
 using System.Collections;
 
 public class Player : MovingObject {
-	
+
 	void Update () {
+		if(!GameController.Instance.isPlayerTurn){
+			return;
+			
+		}
+
 		int xAxis = 0;
 		int yAxis = 0;
 
 		xAxis = (int)Input.GetAxisRaw ("Horizontal");
 		yAxis = (int)Input.GetAxisRaw ("Vertical");
 
-		canObjectMove (xAxis, yAxis);
+		if(xAxis != 0){
+			yAxis = 0;
+
+		}
+
+		if(xAxis != 0 || yAxis !=0){
+		 	canObjectMove (xAxis, yAxis);
+			GameController.Instance.isPlayerTurn = false;
+
+		}
 
 	}
+
 }
+
